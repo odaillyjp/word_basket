f9fa139aa05848772f97ddeb164b73a07594949a
Suguru Odai <ourqwers@gmail.com>
2014-07-26 17:19:59 +0900

    Add firebase class

---
+++ lib/word_basket/database/firebase.rb
@@ -0,0 +1,16 @@
+module WordBasket
+  module Database
+    require 'firebase'
+
+    class Firebase
+      APP_NAME = 'odailly-wordbaskets'
+      BASE_URL = "https://#{APP_NAME}.firebaseio.com/"
+
+      def set(data)
+        firebase = Firebase::Client.new(BASE_URL)
+        response = firebase.set('words', data)
+        response.success?
+      end
+    end
+  end
+end
