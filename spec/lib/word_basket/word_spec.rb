edb0b9182e175d3b9180895de55a03dafb5e0674
Suguru Odai <ourqwers@gmail.com>
2014-07-27 14:48:18 +0900

    Modify some class methods test case to instance methods

--- spec/lib/word_basket/word_spec.rb
+++ spec/lib/word_basket/word_spec.rb
@@ -8,18 +8,6 @@ module WordBasket
 
     # public class methods
 
-    describe '.convert_head_index' do
-      context 'with "パーフェクト"' do
-        it { expect(Word.convert_head_index('パーフェクト')).to eq 'は' }
-      end
-    end
-
-    describe '.convert_last_index' do
-      context 'with "ウォーミングアップ"' do
-        it { expect(Word.convert_last_index('ウォーミングアップ')).to eq 'ふ' }
-      end
-    end
-
     context '"わーどばすけっと", "わーばす", "ばすけっと" saved' do
       before do
         Word.create('わーどばすけっと')
@@ -39,20 +27,6 @@ module WordBasket
         context 'with head "お", last "と"' do
           it { expect(Word.sample(head: 'お', last: 'と')).to be_nil }
         end
-      end
-    end
-
-    # private class methods
-
-    describe '.convert_char_index' do
-      context 'with "ガ"' do
-        it { expect(Word.send(:convert_char_index, 'ガ')).to eq 'か' }
-      end
-    end
-
-    describe '.remove_dakuten' do
-      context 'with "ざ"' do
-        it { expect(Word.send(:remove_dakuten, 'ざ')).to eq 'さ' }
       end
     end
 
@@ -77,6 +51,30 @@ module WordBasket
 
       describe '#to_json' do
         it { expect(word.to_json).to eq '{"name":"あいうえお","head":"あ","last":"お"}' }
+      end
+    end
+
+    describe '#convert_head_index' do
+      context 'with "パーフェクト"' do
+        it { expect(word.send(:convert_head_index, 'パーフェクト')).to eq 'は' }
+      end
+    end
+
+    describe '#convert_last_index' do
+      context 'with "ウォーミングアップ"' do
+        it { expect(word.send(:convert_last_index, 'ウォーミングアップ')).to eq 'ふ' }
+      end
+    end
+
+    describe '#convert_char_index' do
+      context 'with "ガ"' do
+        it { expect(word.send(:convert_char_index, 'ガ')).to eq 'か' }
+      end
+    end
+
+    describe '#remove_dakuten' do
+      context 'with "ざ"' do
+        it { expect(word.send(:remove_dakuten, 'ざ')).to eq 'さ' }
       end
     end
   end
