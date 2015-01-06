042bb1ade1261c2bcf197ff2d5d579913792ef0a
Suguru Odai <ourqwers@gmail.com>
2015-01-06 23:21:41 +0900

    Fix a database option example

--- config/application.example.rb
+++ config/application.example.rb
@@ -1,6 +1,6 @@
 WordBasket.configure do |config|
   config.database_adapter = 'local_file'
-  config.database_options = 'db/words.json'
+  config.database_options = { storage_path: 'db/words.json' }
 
   # Firebaseの場合
   # config.database_adapter = 'firebase'
