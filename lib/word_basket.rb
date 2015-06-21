62715c6687c6fc35cfb580735125969c43088844
Suguru Odai <ourqwers@gmail.com>
2015-06-21 12:14:00 +0900

    Bump version to 1.0.1

--- lib/word_basket.rb
+++ lib/word_basket.rb
@@ -1,11 +1,10 @@
-# WordBasket libraries
 require 'word_basket/core_ext/string'
 require 'word_basket/word'
 require 'word_basket/configuration'
 require 'word_basket/controller'
 
 module WordBasket
-  VERSION = '1.0.0'
+  VERSION = '1.0.1'
 
   def self.configuration
     @configuration ||= WordBasket::Configuration.new
